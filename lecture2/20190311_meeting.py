f5d82ed48e9400438c96a10680f5db779816d1dc
Morbus Borbus <zer00aeon@gmail.com>
2019-03-11 19:13:32 +0200

    teaching my son 4

---
+++ lecture2/20190311_meeting.py
@@ -0,0 +1,71 @@
+ob = "we love python "
+print(type(ob))
+
+numbers = [34, 143, 3, 53, 4]
+print(type(numbers))
+
+x = None
+print(type(x))
+
+countries = {'israel': 'jerusalem', 'italy': 'rome',
+             'france': 'paris'}
+print(countries)
+print(type(countries))
+cars = {'84-185-33': {'color': 'white', 'brand': 'honda', 'model': 'hatchback'},
+        '65-767-22': {'color': 'red', 'brand': 'toyota', 'model': 'corolla'},
+        '43-658-31': {'color': 'blue', 'brand': 'fiat', 'model': 'uno'}}
+
+print(cars['84-185-33'])
+
+# what is set ? set is a collection of UNIQUE objects
+
+numbers = {34, 54, 6, 75, 654, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7}  # definition if a set NOT A DICT !
+print(numbers, type(numbers))
+print(dir(numbers))
+
+import json
+
+options = (dir(json))
+print(type(options))
+
+# why the fuck this works
+for option in options:
+    if not option.find("_"):
+        options.remove(option)
+    else:
+        print(option)
+
+    # print(type(option))
+    #
+# set and frozen set
+names = {'bob', 'alice', 'sasha', 'gal', 45, 45, 34}
+names.add('klaus')
+print(names, "added klaus cause its mutable")
+print(cars.get('84-185-33'))
+print(cars.get('non-Existent-key-return-None'))
+# tuple might be used for database entries in order to eliminate errors
+num = 3.8
+numA = int(num)
+print(numA)
+
+# non decimal numbers
+octo_num = 0o14
+
+print(type(octo_num))
+
+hexanum = 0x14A
+print(hexanum)
+
+bina_num = 0b1001
+print(bina_num)
+starting_number = 14850
+ob = hex(starting_number)
+print(ob)
+ob = bin(starting_number)
+print(ob)
+
+from decimal import Decimal
+
+r = Decimal(0.3) + Decimal(0.12444)
+print(r)
+
--- playground/linux_academy/reverse_file.py
+++ playground/linux_academy/reverse_file.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+
 
 # build parser
 # parse arguments

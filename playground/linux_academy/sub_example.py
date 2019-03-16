54beff11e77d25ad15e5061ea9e2ddfe09f4ee81
Morbus Borbus <zer00aeon@gmail.com>
2019-03-16 19:22:01 +0200

    teaching my son 8

---
+++ playground/linux_academy/sub_example.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+import sys
+
+if os.name == 'nt':
+    proc = subprocess.run(["cmd", "/c", "dir"])
+    print(proc)
+    try:
+        new_proc = subprocess.run(["cmd", "/c", "apop-1"], check=True)
+    # the line above adds a check for process error
+    # print(new_proc)  # CalledProcessError can be caught and handled
+    except subprocess.CalledProcessError as err:
+        print(f"Ahtung !  {err}")
+        print("Make sure that command used exists on current OS ! ")
+        # sys.exit(1)
+
+else:
+    proc = subprocess.run(["ls", "-l"])
+
+print(bytes([255, 146, 246]))  # raw byte information
+
+# print(proc.stdout.decode(bytes([255, 146, 246])))
---
+++ playground/pythonic stuff/smallSel.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome("E:\Development\ebaPo4tu\playground\pythonic stuff\chromewriver-win\chromedriver.exe")
+driver.get("http://python.org")
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "start-shell")))
+    print("Element found!")
+finally:
+    pass
+    # driver.quit()
+
+# implicit vs explicit wait
+# implicit wait - waits before every call (pauses for a certain amount of time for every action
+# implicit can be used if a connection is slow / used once per session
+# explicit wait wait for a certain condition only
+
+driver.implicitly_wait(10)
+driver.get("http://python.org")
+we = driver.find_element_by_id('start-shell')
+driver.close()

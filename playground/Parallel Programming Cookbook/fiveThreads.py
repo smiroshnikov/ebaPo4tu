f457cd3eeca9bdf77b60eb584c2409e2de106f2a
Sergeim <smiroshnikov@securitydam.com>
2019-03-25 16:51:59 +0200

    book examples

--- playground/Parallel Programming Cookbook/fiveThreads.py
+++ playground/Parallel Programming Cookbook/fiveThreads.py
@@ -1,19 +1,43 @@
 import threading
 import time
+from random import randint
+from datetime import timedelta
 
 
 def megafun(i):
+    start = time.time()
     print(f"function called by thread {i}")
     result = i ** i ** i
     print(f"meaningless result is {result} time is {time.time()}")
+    print(f"{threading.current_thread()}")
+    end = time.time()
+    elapsed = end - start
+    print(f"{str(timedelta(seconds=elapsed))}")
+
     return
+
+
+def yubaniy_nasos(i):
+    start = time.time()
+    # print(f"function called by thread {i}")
+    # print(f"{threading.current_thread()}")
+    time.sleep(randint(1, 20))
+    end = time.time()
+    elapsed = end - start
+    print(f"{str(timedelta(seconds=elapsed))} : elapsed time for {threading.current_thread()}" )
+
 
 
 threads = []
 for i in range(5):
-    t = threading.Thread(target=megafun,
+    # t = threading.Thread(target=megafun,
+    #                      args=(i,)
+    #                      )
+    t = threading.Thread(target=yubaniy_nasos,
                          args=(i,)
                          )
+
     threads.append(t)
     t.start()
-    t.join()
+    # t.join()
+
---
+++ playground/Parallel Programming Cookbook/suspendExecution.py
@@ -0,0 +1,5 @@
+import threading
+import time
+
+def first_function():
+    print(threading.current_thread())

4f5407a0fefff4c51da630c06351109d5cf8b2aa
Sergeim <smiroshnikov@securitydam.com>
2019-03-27 21:12:51 +0200

    nested lists

--- playground/prep/notAparser.py
+++ playground/prep/notAparser.py
@@ -2,7 +2,21 @@ lines = []
 
 with open('cities.csv', 'r') as f:
     for line in f:
-        fields = (line.split(','))
+        # fields = (line.split(','))
+        fields = (line)
         # lines.append(f.readline())
         lines.append(fields)
-print([value.strip('\n') for line in lines for value in line])
+
+print([l for l in lines])
+print(f"{len(lines)}")
+# print([value.strip('\n') for line in lines for value in line])
+
+
+
+class Foo:
+    def __call__(self, *args, **kwargs):
+        print("called")
+
+
+foo_instance = Foo()
+foo_instance()

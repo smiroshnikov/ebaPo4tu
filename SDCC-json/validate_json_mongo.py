a8eebd43c6beacefdfa66f126d3287b168bb308c
Sergeim <smiroshnikov@securitydam.com>
2019-06-04 10:04:52 +0300

    added json for testing

---
+++ SDCC-json/validate_json_mongo.py
@@ -0,0 +1,12 @@
+import json
+from pymongo import MongoClient
+
+with open("data-assetRoutes.json", 'r') as f:
+    objs = json.load(f)
+
+    client = MongoClient('10.20.4.30', 27017)
+    db = client.sdcc
+    for o in objs:
+        db.AssetRoutes.insert_one(o)
+
+print ("fuck ")
